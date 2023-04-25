28580af7b740eee2763822209c91e05026d796a3
Ethiopia Coconut <373678768@qq.com>
2023-04-25 15:01:18 +0800

    Update CheckinSuite.js

--- CheckinSuite.js
+++ CheckinSuite.js
@@ -8,6 +8,7 @@
 // @match        https://hifini.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=alibaba-inc.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
