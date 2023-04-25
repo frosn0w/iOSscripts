8213d19dba77c6bc64221a2ebfbf578268bda174
Ethiopia Coconut <373678768@qq.com>
2023-04-25 14:56:19 +0800

    Update AliCheckin.js

--- AliCheckin.js
+++ AliCheckin.js
@@ -1,30 +1,31 @@
 // ==UserScript==
-// @name         AliLearn Checkin
+// @name         Kelvin's Checkin Suite
 // @namespace    https://github.com/frosn0w/iOSscripts
-// @version      1.0.1
-// @description  Alibaba Product checkin
+// @version      1.1
+// @description  Checkin all in one
 // @author       frosn0w
-// @match        https://xue.alibaba-inc.com/trs/xue/home
+// @match        https://xue.alibaba-inc.com/trs/xue/home*
+// @match        https://hifini.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=alibaba-inc.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
     // 定义一个定时器，每隔一秒检测元素是否存在
     var timer = setInterval(function(){
-        // 获取所有span元素中，class=next-btn-helper的元素
-        var spans = document.querySelectorAll("span.next-btn-helper");
-        // 遍历这些元素
+        // find all spans
+        var spans = document.querySelectorAll("span");
         for (var i = 0; i < spans.length; i++) {
-            // 如果元素的文本内容为“签到”
-            if (spans[i].innerText === "签到") {
-                // 点击元素
+            //alilearn
+            if (spans[i].getAttribute("class") === "next-btn-helper" || spans[i].innerText === "签到") {
                 spans[i].click();
-                // 清除定时器，不再执行
-                clearInterval(timer);
             }
+            //hifini
+            else if (spans[i].getAttribute("class") === "mibbs_signpanel JD_sign " || spans[i].innerText === "签到") {
+                spans[i].click();
+            }
+            else continue;
         }
-    }, 1000);
+    }, 1500);
 })();
