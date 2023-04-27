0c20c4bbd28b4cbeed87c71d939d691dd50ac7bb
Ethiopia Coconut <373678768@qq.com>
2023-04-27 10:54:38 +0800

    Create CheckinSuite.js

--- CheckinSuite.js
+++ CheckinSuite.js
@@ -1,30 +1,35 @@
 // ==UserScript==
 // @name         CheckinSuite
 // @namespace    https://github.com/frosn0w/iOSscripts
-// @version      1.2
+// @version      1.2.1
 // @description  Checkin all in one
 // @author       frosn0w
 // @match        https://xue.alibaba-inc.com/trs/xue/home*
 // @match        https://hifini.com
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=alibaba-inc.com
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAAFVBMVEVHcEwzMzIzMzI6OjYnJywzMzIzMzJtKzdZAAAAB3RSTlMAzf8CArThzF+FCQAAAERJREFUeAFjAAJGATiGACYFGMYqYKiEAkQYglAFVBmUULUoYRVgS4BhqAACkC/ApKTAoKQEoggLDHVDKRfAjDlWVIEAALtyGcbXf1bkAAAAAElFTkSuQmCC
+// @run-at       document-end
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
-    // 定义一个定时器，每隔一秒检测元素是否存在
+    // Define timer
     var timer = setInterval(function(){
         // find all spans
         var spans = document.querySelectorAll("span");
+        var divs = document.querySelectorAll("div");
         for (var i = 0; i < spans.length; i++) {
             //alilearn
             if (spans[i].getAttribute("class") === "next-btn-helper" && spans[i].innerText === "签到") {
                 spans[i].click();
             }
+            else continue;
+        }
+        for (var j = 0; j < divs.length; j++){
             //hifini
-            else if (spans[i].getAttribute("class") === "mibbs_signpanel JD_sign " && spans[i].innerText === "签到") {
-                spans[i].click();
+            if (divs[j].getAttribute("id") === "sign" && divs[j].innerText === "签到") {
+                divs[j].click();
             }
             else continue;
         }
